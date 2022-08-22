b7160669a55f9bdde2e3fd00a377880e26fff19c
mlights999 <mlights999@gmail.com>
2022-08-22 19:38:02 -0400

    Fix compile

---
+++ WaterNet23PreAlpha/target/1.5.2/bsom/obj/src/user_module.o.d
@@ -0,0 +1,8 @@
+c:/Users/mligh/OneDrive/Particle/WaterNet23/WaterNet23PreAlpha/target/1.5.2/bsom//obj/./src/user_module.o: \
+ src/user_module.c \
+ ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
+ ../../../dynalib/inc/dynalib.h
+
+../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
+
+../../../dynalib/inc/dynalib.h:
